a2f5af7a0b0a1c994a0e9a0238f3288fee625df9
NycolasBC <nycolascarvalho14@gmail.com>
2023-09-26 19:32:00 -0300

    first commit

---
+++ Controllers/CartasController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Prova_Parte01.Model.Request;
+using System;
+using WebApplicationList01.Controllers;
+
+namespace MegaSena.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CartasController : PrincipalController
+    {
+        #region Propriedades e construtor
+
+        private readonly string _cartasCaminhoArquivo;
+
+        public CartasController()
+        {
+            _cartasCaminhoArquivo = Path.Combine(Directory.GetCurrentDirectory(), "Data", "cartas.json");
+        }
+
+        #endregion
+
+        #region Métodods Arquivo
+
+        private List<CartasViewModel> LerCartaArquivo()
+        {
+            if (!System.IO.File.Exists(_cartasCaminhoArquivo))
+            {
+                return new List<CartasViewModel>();
+            }
+
+            string json = System.IO.File.ReadAllText(_cartasCaminhoArquivo);
+            return JsonConvert.DeserializeObject<List<CartasViewModel>>(json);
+        }
+
+        private int ObterProximoCodigoDisponivel()
+        {
+            List<CartasViewModel> cartas = LerCartaArquivo();
+            if (cartas.Any())
+            {
+                return cartas.Max(j => j.Id) + 1;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        private void EscreverNoArquivo(List<CartasViewModel> cartas)
+        {
+            string json = JsonConvert.SerializeObject(cartas);
+            System.IO.File.WriteAllText(_cartasCaminhoArquivo, json);
+        }
+
+        #endregion
+
+        #region Metodos CRUD
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<CartasViewModel> cartas = LerCartaArquivo();
+            return Ok(cartas);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] NovasCartasViewModel novasCartas)
+        {
+            if (!ModelState.IsValid)
+            {
+                return ApiBadRequestResponse(ModelState);
+            }
+
+            List<CartasViewModel> cartas = LerCartaArquivo();
+
+            int proximoCodigo = ObterProximoCodigoDisponivel();
+
+            CartasViewModel novaCarta = new CartasViewModel()
+            {
+                Id = proximoCodigo,
+                Nome = novasCartas.Nome,
+                EnderecoCarta = novasCartas.EnderecoCarta,
+                Idade = novasCartas.Idade,
+                TextoCarta  = novasCartas.TextoCarta
+            };
+
+            cartas.Add(novaCarta);
+            EscreverNoArquivo(cartas);
+
+            return ApiResponse(cartas, "Carta registrada com sucesso.");
+        }
+
+        #endregion
+    }
+}
---
+++ Model/Request/NovasCartasViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Prova_Parte01.Model.Request
+{
+    public class NovasCartasViewModel
+    {
+        [JsonIgnore]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "É necessário ter um nome.")]
+        [MinLength(3, ErrorMessage = "É necessário ter no minimo 3 caracteres."), MaxLength(255, ErrorMessage = "Limite máximo de 255 caracteres.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "É necessário informa um endereço.")]
+        public Endereco EnderecoCarta { get; set; }
+
+        [Required(ErrorMessage = "É necessário escrever uma carta.")]
+        [Range(1, 14, ErrorMessage = "Informe uma idade de 1 a 14 anos.")]
+        public int Idade { get; set; }
+
+        [Required(ErrorMessage = "É necessário escrever uma carta.")]
+        [MaxLength(500, ErrorMessage = "Limite máximo de 500 caracteres.")]
+        public string TextoCarta { get; set; }
+    }
+
+    public class Endereco
+    {
+        public string Rua { get; set; }
+        public int Numero { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string UF { get; set; }
+    }
+}
